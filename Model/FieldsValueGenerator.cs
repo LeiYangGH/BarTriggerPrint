6d785fa20bdb0dbdcddbc02b0bab41b86da8da20
yang.lei <yang.lei@nuance.com>
2019-05-16 22:49:21 +0800

    draft format gen

---
+++ Model/FieldsValueGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarTriggerPrint.Model
+{
+    public abstract class FieldsValueGenerator
+    {
+        protected string dateFormatString = "yyMMdd";
+        protected int sNLength = 4;
+        protected string componentType;
+        public FieldsValueGenerator(string componentType)
+        {
+            this.componentType = componentType;
+        }
+
+        public string DirName { get; protected set; }
+        public string GetFormatedDateString(DateTime dt)
+        {
+            return dt.ToString(this.dateFormatString);
+        }
+        public string GetFormatedSNString(int num)
+        {
+            int toMod = (int)Math.Floor(Math.Pow(10, this.sNLength));
+            return (num % toMod).ToString().PadLeft(this.sNLength, '0');
+        }
+    }
+}
---
+++ Model/No066Generator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarTriggerPrint.Model
+{
+    public class No066Generator : FieldsValueGenerator
+    {
+        public No066Generator() : base("NF前")
+        {
+            this.DirName = "NF前";
+        }
+
+        private string GetYearCharString(int year)
+        {
+            if (year <= 2019)
+                return (year - 2010).ToString();
+            else if (year >= 2020 && year <= 2027)
+            {
+                return ((char)(year - 2020 + 64)).ToString();
+            }
+            else
+                return "BADYEAR";
+        }
+
+        private string GetMonthCharString(int m)
+        {
+            if (m <= 9)
+                return m.ToString();
+            else if (m >= 10 && m <= 12)
+            {
+                return ((char)(m - 10 + 64)).ToString();
+            }
+            else
+                return "BADMONTH";
+        }
+
+
+        private string GetDayCharString(int m)
+        {
+            if (m <= 9)
+                return m.ToString();
+            else if (m == 10)
+            {
+                return "0";
+            }
+            else if (m >= 11 && m <= 31)
+            {
+                return ((char)(m - 11 + 64)).ToString();
+            }
+            else
+                return "BADDAY";
+        }
+    }
+}
