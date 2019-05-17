8eae466bd3929fba28e7d1bfab3aec9a47ee0bf4
yang.lei <yang.lei@nuance.com>
2019-05-17 17:12:18 +0800

    temp commit save

--- Model/FieldsValueGenerator.cs
+++ Model/FieldsValueGenerator.cs
@@ -10,21 +10,36 @@ namespace BarTriggerPrint.Model
     {
         protected string dateFormatString = "yyMMdd";
         protected int sNLength = 4;
-        protected string componentType;
-        public FieldsValueGenerator(string componentType)
+
+        public virtual string ConvertToValue<T>(T input) where T : struct
         {
-            this.componentType = componentType;
+            //if (typeof(T) == typeof(DateTime))
+            if (input is DateTime)
+            {
+                DateTime dt = (DateTime)Convert.ChangeType(input, typeof(DateTime));
+                return this.ConvertDate(dt);
+            }
+            else if (input is int)
+            {
+                int dt = (int)Convert.ChangeType(input, typeof(int));
+                return this.ConvertSn(dt);
+            }
+            else
+                return "WRONGTYPE";
         }
 
-        public string DirName { get; protected set; }
-        public string GetFormatedDateString(DateTime dt)
+
+        protected virtual string ConvertDate(DateTime dt)
         {
             return dt.ToString(this.dateFormatString);
         }
-        public string GetFormatedSNString(int num)
+
+        protected virtual string ConvertSn(int num)
         {
             int toMod = (int)Math.Floor(Math.Pow(10, this.sNLength));
             return (num % toMod).ToString().PadLeft(this.sNLength, '0');
         }
+
+
     }
 }
